dd4566d4cb7e2c5892f41c2299fbd342350de610
Mohammd Mustafa <momust187@gmail.com>
2022-01-18 20:47:02 +0100

    Navbar responsiv

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,21 +6,25 @@ import  '../Css/NavStyle.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function Nav() {
     return (
-     <header>
-         <div className='container container-flex row'>
-            <nav className='col-lg-12 ms-5' >
-                <div className='nav-bar '>
+        <html>
+            <header>
+         <div className='container '>
+            <nav>
+                <div className='nav-bar'>
                     <ul>
-                        <NavLink to="/" className='nav-items' activeClassName='active-item'>Home</NavLink>
+                        <NavLink exact to="/" className='nav-items' activeClassName='active-item'>Home</NavLink>
                         <NavLink to="/About" className='nav-items' activeClassName='active-item'>About</NavLink>
                         <NavLink to="/MyWork" className='nav-items' activeClassName='active-item'>My work</NavLink>
                         <NavLink to="/Experience" className='nav-items' activeClassName='active-item'>Experience</NavLink>
-                        <NavLink to="/Experience" className='nav-items-right ms-auto' activeClassName='active-item'>Contact</NavLink>
+                        <NavLink to="/Contact" className='nav-items-right ms-auto' activeClassName='active-item'>Contact</NavLink>
+                        
                     </ul>
                 </div>
             </nav>
         </div>
      </header>
+        </html>
+     
     )
 }
 
