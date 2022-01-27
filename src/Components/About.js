7ee9db31b22c3377e8a0e2bbacef7743243bb186
Mohammd Mustafa <momust187@gmail.com>
2022-01-27 18:48:08 +0100

    About page: profile done

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,6 +6,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import SchoolIcon from '@mui/icons-material/School';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
 function About() {
     
@@ -13,42 +14,46 @@ function About() {
         <div className='body-class-about'>
             <div className='wrapper'>
                  <div className='row row-custom d-flex justify-content-center mt-4 '>
-                <div className='profile-card col-lg-3 col-md-5 col-sm-8 col-8'>
-                    <div className='profile-bg'>
-                        <div className='profile-image'>
-                            <img src={Pic}></img>
-                        </div>
-                        
-                    </div>
-                    <div className='profile-card-text'>
-                        <h5>Mohammed Mustafa</h5>
-                    </div>
-                    <div className='profile-card-text'>
-                        <h5>Front-end / Web Developer</h5>
-                    </div>
-                    <div className='social-media-about'>
-                        <a href="#"><FacebookIcon className='social-icons' variant="contained"  color="primary"/></a>
-                        <a href="#"><LinkedInIcon   className='social-icons' variant="contained" color="primary"/></a>
-                    </div>
-                    <div className='profile-info'>
-                        <div className='profile-info-details'>
-                            <SchoolIcon /><p>KYH Stockholm</p>
+                    <div className='profile-card col-lg-3 col-md-5 col-sm-8 col-8'>
+                        <div className="profile-info-container">
+                            <div className='profile-bg'>
+                            <div className='profile-image'>
+                                <img src={Pic}></img>
+                            </div>
                             
                         </div>
-                        <div className='profile-info-details'>
-                            < LocationOnIcon /><p>KYH Stockholm</p>
+                        <div className='profile-card-text'>
+                            <h5>Mohammed <span className='title-color'>Mustafa</span></h5>
                         </div>
-                        <div className='profile-info-details'>
-                            <SchoolIcon /><p>KYH Stockholm</p>
+                        <div className='profile-card-text'>
+                            <h5>Front-end / Web Developer</h5>
+                        </div>
+                        <div className='social-media-about'>
+                            <a href="#"><FacebookIcon className='social-icons' variant="contained"  color="primary"/></a>
+                            <a href="#"><LinkedInIcon   className='social-icons' variant="contained" color="primary"/></a>
+                        </div>
+                        <div className='profile-info'>
+                            <div className='profile-info-details'>
+                                <SchoolIcon /><p>KYH Stockholm</p>
+                                
+                            </div>
+                            <div className='profile-info-details'>
+                                < LocationOnIcon /><p>Stockholm, Sweden</p>
+                            </div>
+                            <div className='profile-info-details'>
+                                <CalendarTodayIcon /><p>June 11 1993</p>
+                            </div>
+                        </div>
+                        <div className='send-mail'>
+                            <button type='button' className='btn-primary'>Email me</button>
+                        </div>
                         </div>
                     </div>
-                     
-                </div>
                
-                <div className='about-info col-lg-5 col-md-5 col-sm-10 col-10'>
-                    <p>col.2</p>
+                    <div className='about-info col-lg-5 col-md-5 col-sm-10 col-10'>
+                        <p>col.2</p>
+                    </div>
                 </div>
-            </div>
             </div>
            
         </div>
